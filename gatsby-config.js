0a8fb346ee0e2a1f68e4c0f203f3e29e96859751
koji Ishimoto <koji.programs@gmail.com>
2019-08-29 18:33:05 +0900

    Add connpass events page

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-react-helmet',
+    `gatsby-plugin-postcss`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -19,6 +20,49 @@ module.exports = {
       },
     },
     'gatsby-plugin-sass',
-    'gatsby-plugin-offline'
+    'gatsby-plugin-offline',
+    {
+      resolve: "gatsby-source-custom-api",
+      options: {
+          url: "https://connpass.com/api/v1/event/?series_id=7278&order=2&count=100",
+          rootKey: "connpass",
+          schemas: {
+            connpass: `
+              results_returned: Int
+              results_available: Int
+              results_start: Int
+              events: [events]
+            `,
+            events: `
+              event_id: Int
+              title: String
+              catch: String
+              description: String
+              event_url: String
+              hash_tag: String
+              started_at: String
+              ended_at: String
+              limit: Int
+              event_type: String
+              series: [series]	
+              address: String
+              place: String
+              lat: Float
+              lon: Float
+              owner_id: Int
+              owner_nickname: String
+              owner_display_name: String
+              accepted: Int
+              waiting: Int
+              updated_at: String
+            `,
+            series: `
+              id: Int
+              title: String
+              url: String
+            `
+        }
+      }
+    }
   ],
 }
