b00688f7c53c27e262de9835196b23ccb424a1b6
koji Ishimoto <koji.programs@gmail.com>
2019-08-30 10:39:41 +0900

    Style events page and add transition link

--- src/pages/events.js
+++ src/pages/events.js
@@ -1,15 +1,21 @@
 import React from 'react'
-import { Link, useStaticQuery, graphql } from "gatsby"
+import { useStaticQuery, graphql } from "gatsby"
+import AniLink from "gatsby-plugin-transition-link/AniLink"
+
+import { parseISO, format } from 'date-fns'
+import { ja } from 'date-fns/locale'
 
 const Events = (props) => {
   const events = props.data.connpass.events
   console.log(events)
   return (
-    <div>
+    <div className="my-4">
+      <h1>開催したもくもく会・勉強会</h1>
       {events.map(event => {
         return (
-          <li key={event.event_id} className="list-none">
-            <Link to={`/events/${event.event_id}`}>{event.title}</Link>
+          <li key={event.event_id} className="list-none my-8">
+            <h3>{format(parseISO(event.started_at), 'PPP EEEE', { locale: ja })}</h3>
+            <AniLink fade to={`/events/${event.event_id}`}>{event.title}</AniLink>
           </li>  
         )
       })}
@@ -24,6 +30,7 @@ export default () => {
         events {
           title
           event_id
+          started_at
         }
       }
     }
