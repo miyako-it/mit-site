25daeda7a60ba4f18a24119815718d2fd6a34b4f
koji Ishimoto <koji.programs@gmail.com>
2019-10-31 00:52:55 +0900

    ✨ add details of event information in event page

--- src/templates/event.js
+++ src/templates/event.js
@@ -4,6 +4,8 @@ import { graphql } from 'gatsby'
 import Seo from 'components/Seo'
 
 import insane from 'insane' //xss対策
+import { parseISO, format } from 'date-fns'
+import { ja } from 'date-fns/locale'
 
 export default function Template({ data }) {
   const events = data.events
@@ -12,8 +14,35 @@ export default function Template({ data }) {
   return (
     <div className="max-w-5xl mx-auto px-8 md:px-24">
       <Seo title={events.title} />
-      <h1 className="my-8 font-sans font-bold text-2xl">{events.title}</h1>
+      <h1 className="my-8 font-sans font-bold text-2xl">
+        <a
+          className="underline_center"
+          href={events.event_url}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {events.title}
+        </a>
+      </h1>
       <h2 className="my-4 font-sans text-md">{events.catch}</h2>
+      <h3 className="mt-8 font-sans text-sm">
+        <span>開催日時 ： </span>
+        {format(parseISO(events.started_at), `PPP p`, {
+          locale: ja,
+        })}
+        <span> 〜 </span>
+        {format(parseISO(events.ended_at), `p EEEE`, {
+          locale: ja,
+        })}
+      </h3>
+      <h4 className="font-sans text-sm">
+        <span>会場 ： </span>
+        {events.place}
+      </h4>
+      <h4 className="font-sans text-sm">
+        <span>住所 ： </span>
+        {events.address}
+      </h4>
       <article
         className="mt-16 font-serif break-all markdown-body"
         dangerouslySetInnerHTML={{ __html: bodyHtml }}
@@ -27,8 +56,9 @@ export const pageQuery = graphql`
       event_id
       title
       started_at
+      ended_at
       place
-      limit
+      address
       event_url
       catch
       description
