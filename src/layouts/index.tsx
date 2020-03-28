edf85391871d7f6d54fb09539a00c319b3e8ee70
Koji Ishimoto <koji.ishimoto@gmail.com>
2020-03-29 00:42:17 +0900

    Add chakra-ui

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -3,12 +3,18 @@ import React from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 
+import { ThemeProvider } from "@chakra-ui/core"
+import { theme } from "@chakra-ui/core"
+
+// Use at the root of your app
 const Layout = ({ children }) => (
-  <div className="bg-neutral min-h-screen flex flex-col">
-    <Header />
-    <main className="flex-1">{children}</main>
-    <Footer />
-  </div>
+  <ThemeProvider theme={theme}>
+    <div className="bg-neutral min-h-screen flex flex-col">
+      <Header />
+      <main className="flex-1">{children}</main>
+      <Footer />
+    </div>
+  </ThemeProvider>
 )
 
 export default Layout
