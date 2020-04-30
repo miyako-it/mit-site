ce7a5bb76a621256909558367cee4f6bfd4a1985
Koji Ishimoto <koji.ishimoto@gmail.com>
2020-04-30 21:09:58 +0900

    Use type assertion for fluid object

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { graphql } from 'gatsby'
 
 import AniLink from 'gatsby-plugin-transition-link/AniLink'
-import Img from 'gatsby-image'
+import Img, { FluidObject } from 'gatsby-image'
 import Seo from '../components/Seo'
 import { IndexPageQuery } from "../../types/graphql-types"
 import Helmet from 'react-helmet'
@@ -23,13 +23,13 @@ const UpcomingEvents = ({ events, className }) => {
   } else {
     return (
       <div className={className}>
-        <h2 className="font-sans font-bold text-namari text-2xl text-left">開催予定</h2>
-        <ul className="mt-16 font-serif text-namari text-lg text-left">
+        <h2 className="font-sans text-2xl font-bold text-left text-namari">開催予定</h2>
+        <ul className="mt-16 font-serif text-lg text-left text-namari">
           {upcomingEvents.map(event => {
             return (
               <li
                 key={event.event_id}
-                className="list-none my-8 last:my-0 last:mt-8 flex flex-col items-start"
+                className="flex flex-col items-start my-8 list-none last:my-0 last:mt-8"
               >
                 <time dateTime={event.started_at} className="mb-2">
                   {format(parseISO(event.started_at), `PPP EEEE`, {
@@ -52,30 +52,30 @@ const UpcomingEvents = ({ events, className }) => {
   }
 }
 const IndexPage : React.FC<Props> = ({ data }) => {
-  const fluid = data.allImageSharp.edges[0].node.fluid
+  const fluid = data.allImageSharp.edges[0].node.fluid as FluidObject
   const upcomingEvents = data.connpass.events
   return (
     <div className="px-8 md:px-24">
-      <div className=" mx-auto h-screen -mt-12 md:-mt-24 flex items-center justify-betweeen">
+      <div className="flex items-center h-screen mx-auto -mt-12 md:-mt-24 justify-betweeen">
         <Seo title="MIT" />
         <div className="max-w-md lg:w-5/12">
-          <h1 className="w-full font-sans font-bold text-namari text-4xl text-left leading-loose tracking-widest">
+          <h1 className="w-full font-sans text-4xl font-bold leading-loose tracking-widest text-left text-namari">
             みやこIT勉強会はもくもく会を毎月土曜日に開催しています
           </h1>
         </div>
         <Img
-          className="hidden lg:block ml-auto -mr-8 md:-mr-24 xl:-mr-56 self-start w-8/12 xl:w-9/12 h-full border-b-24 border-neutral"
+          className="self-start hidden w-8/12 h-full ml-auto -mr-8 lg:block md:-mr-24 xl:-mr-56 xl:w-9/12 border-b-24 border-neutral"
           fluid={fluid}
           alt="会場の写真"
         />
       </div>
       <Img
-        className="lg:hidden max-w-screen h-full mb-12 -mx-8 md:-mx-24"
+        className="h-full mb-12 -mx-8 lg:hidden max-w-screen md:-mx-24"
         fluid={fluid}
         alt="会場の写真"
       />
-      <UpcomingEvents className="mx-auto pt-10 mb-24" events={upcomingEvents} />
-      <h2 className="mb-12 font-sans font-bold text-namari text-2xl text-left">Twitter</h2>
+      <UpcomingEvents className="pt-10 mx-auto mb-24" events={upcomingEvents} />
+      <h2 className="mb-12 font-sans text-2xl font-bold text-left text-namari">Twitter</h2>
       <a className="twitter-timeline" data-height="600" href="https://twitter.com/AIAI03304476?ref_src=twsrc%5Etfw"></a>
     </div>
   )
